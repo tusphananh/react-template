4c57ebf617dff317f16ac8d13d9b89ac56913b52
Tu Phan <tusphananh@gmail.com>
2021-06-16 11:48:09 +0700

    update

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 
 const express = require("express");
 const app = express();
+const cors = require("cors");
+app.use(cors());
 
 app.get("/api/key",(req, res) => {
     res.send(JSON.stringify({ 
@@ -9,7 +11,7 @@ app.get("/api/key",(req, res) => {
     password : "test1107"}));
 })
 // Port Environment variable
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3060;
 
 // Creating the node server
 app.listen(PORT, () => {
