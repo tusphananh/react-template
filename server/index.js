236d7b1894831aba085096e1cc51dc85e15d8870
Tu Phan <tusphananh@gmail.com>
2021-06-17 10:58:24 +0700

    update

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const app = express();
 const cors = require("cors");
 app.use(cors());
 
-app.get("/api/key",(req, res) => {
+app.get("/api/key",'localhost',(req, res) => {
     res.send(JSON.stringify({ 
     ws : "wss://sbc03.tel4vn.com:7444",
     uri  : "107@2-test1.gcalls.vn:50061",
