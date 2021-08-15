9c5bd420ff9683a802d21aa30bb25c1cafdfca29
Tu Phan <tusphananh@gmail.com>
2021-08-15 16:24:37 +0700

    update

--- server/index.js
+++ server/index.js
@@ -1,13 +1,30 @@
-
 const express = require("express");
-const app = express();
+const http = require("http");
 const cors = require("cors");
-app.use(cors());
+const mongoose = require("mongoose");
+require("dotenv")
 
-// Port Environment variable
-const PORT = process.env.PORT || 3060;
+const db = async (req, res) => {
+  try {
+    console.log("Connecting to MongoDB");
+    const uri = `mongodb+srv://admin:${process.env.MONGO_PASSWORD}@tuphan.id0lo.mongodb.net/react?retryWrites=true&w=majority`;
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
-// Creating the node server
-app.listen(PORT,'localhost', () => {
-    console.log(`Example app listening at http://localhost:${PORT}`)
-  })
+db()
+
+const port = process.env.PORT || 5000;
+const app = express();
+const server = http.createServer(app);
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
