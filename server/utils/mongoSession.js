a8300d12c7ac5c91a74a6faee76e8a52bf013528
Tu Phan <tusphananh@gmail.com>
2021-08-23 20:16:50 +0700

    update

--- server/utils/mongoSession.js
+++ server/utils/mongoSession.js
@@ -15,6 +15,7 @@ const mongoSession = () => {
       cookie: {
         secure: true, // if true only transmit cookie over https
         httpOnly: true, // if true prevent client side JS from reading the cookie
+        domain: '.herokuapp.com', // if true only transmit cookie over https
         maxAge: 1000 * 60 * 10, // session max age in miliseconds
       },
     });
