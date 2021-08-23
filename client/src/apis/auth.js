d82d5ac70661ac84756fff0919d3db5c3f893587
Tu Phan <tusphananh@gmail.com>
2021-08-23 19:56:11 +0700

    update

--- client/src/apis/auth.js
+++ client/src/apis/auth.js
@@ -1,28 +1,24 @@
 import axios from "axios";
 axios.defaults.withCredentials = true;
 export const requestLogin = (phone) => {
-  const headers = {
-    "Access-Control-Allow-Origin": "*",
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
-    "Access-Control-Allow-Headers":
-      "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
-  };
-  const data = { phone: phone };
-  return axios.post(`${process.env.REACT_APP_API_HOST}/api/auth/login`, data, {
-    headers: headers,
-  });
-};
+    const headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      };
+      const data = { phone: phone };
+      return axios
+        .post(`${process.env.REACT_APP_API_HOST}/api/auth/login`, data, {
+          headers: headers,
+        })
+}
 
 export const requestDasboard = () => {
-  const headers = {
-    "Access-Control-Allow-Origin": "*",
-    "Content-Type": "application/json",
-    "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
-    "Access-Control-Allow-Headers":
-      "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
-  };
-  return axios.post(`${process.env.REACT_APP_API_HOST}/api/auth/dashboard`, {
-    headers: headers,
-  });
-};
+    const headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json",
+      };
+      return axios
+        .post(`${process.env.REACT_APP_API_HOST}/api/auth/dashboard`, {
+          headers: headers,
+        })
+}
