52d06d532bda500a2673b2e4ead5f487489c088b
Tu Phan <tusphananh@gmail.com>
2021-08-23 20:10:42 +0700

    update

--- server/utils/mongoSession.js
+++ server/utils/mongoSession.js
@@ -13,7 +13,7 @@ const mongoSession = () => {
       resave: false,
       saveUninitialized: false,
       cookie: {
-        secure: false, // if true only transmit cookie over https
+        secure: true, // if true only transmit cookie over https
         httpOnly: true, // if true prevent client side JS from reading the cookie
         maxAge: 1000 * 60 * 10, // session max age in miliseconds
       },
